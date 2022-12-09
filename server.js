abb6189db51c67bf6de5e03278bd5fa2b32f034b
lapivoinerouge <wioladys@gmail.com>
2022-12-09 16:09:36 +0100

    Add contact form and handle file upload

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer')
+const upload = multer({ dest: 'public/' })
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -23,6 +27,21 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact');
 });
+
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const design = req.file;
+
+  if(author && sender && title && message && design) {
+    res.render('contact', { isSent: true, filePath: design.filename });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
+
 
 app.get('/info', (req, res) => {
   res.render('info');
